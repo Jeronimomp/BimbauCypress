ca6bfbeb0667285c6c66b43ca04fa9ade70813b1
angemase <angel.marzola@sofka.com.co>
2021-09-10 09:31:19 -0500

    update 3

--- cypress/integration/RegistroBimbau.js
+++ cypress/integration/RegistroBimbau.js
@@ -2,29 +2,28 @@
    /// <reference types="cypress" /> 
    describe('Registro', function(){
    
-    it('Registro exitoso en Bimbau', function(){
+    it('Registro exitoso en Bimbau', function(){cy.wait(2000);
     cy.visit('https://app.bimbau.co/');
-    
-    cy.get(':nth-child(4) > .header-container > .login-container > .btn-register').click();
+    cy.wait(2000);
+    cy.get(':nth-child(4) > .header-container > .login-container > .btn-register').click();cy.wait(2000);
     cy.get('.toast__close-button > svg').click();//cerrar pop up de cookies
-    cy.get(':nth-child(1) > .field > .ui > input').type('angeldavid55@gmail.com');
-    cy.get(':nth-child(2) > .field > .ui').click({force: true});
-    cy.get('.visible > :nth-child(2) > .text').click({force: true});
-    cy.get(':nth-child(3) > .field > .ui > input').type('1063283967',{force: true});
-    cy.get(':nth-child(4) > .field > .ui > input').type('Angel',{force: true});
-    cy.get(':nth-child(5) > .field > .ui > input').type('Marzola',{force: true});
-    cy.get('.equal > :nth-child(1) > input').type('3127016780',{force: true});
-    cy.get('.equal > :nth-child(2) > input').type('7746670',{force: true});
-    cy.get(':nth-child(3) > input').type('333',{force: true});
-    cy.get('.mt-4 > .field > .ui > input').type('Vendedor',{force: true});
+    cy.wait(2000);
+    cy.get(':nth-child(1) > .field > .ui > input').type('angeldavid55@gmail.com');cy.wait(2000);
+    cy.get(':nth-child(2) > .field > .ui').click({force: true});cy.wait(2000);
+    cy.get('.visible > :nth-child(2) > .text').click({force: true});cy.wait(2000);
+    cy.get(':nth-child(3) > .field > .ui > input').type('1063283967',{force: true});cy.wait(2000);
+    cy.get(':nth-child(4) > .field > .ui > input').type('Angel',{force: true});cy.wait(2000);
+    cy.get(':nth-child(5) > .field > .ui > input').type('Marzola',{force: true});cy.wait(2000);
+    cy.get('.equal > :nth-child(1) > input').type('3127016780',{force: true});cy.wait(2000);
+    cy.get('.equal > :nth-child(2) > input').type('7746670',{force: true});cy.wait(2000);
+    cy.get(':nth-child(3) > input').type('333',{force: true});cy.wait(2000);
+    cy.get('.mt-4 > .field > .ui > input').type('Vendedor',{force: true});cy.wait(2000);
   
 
     function cambiar(){
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-       // document.getElementById('recaptcha-token').setAttribute('type', 'text');
-        $("#recaptcha-token").prop("type", "text");
+      
         };
-    cy.wait(500);
+    cy.wait(2000);
   
    cy.get('iframe')
     .first()
