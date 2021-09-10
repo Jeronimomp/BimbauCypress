7fabcf59db59ede0a74febf28fc3d4832d35eca4
angemase <angel.marzola@sofka.com.co>
2021-09-10 15:50:16 -0500

    se borra función que intentaba deshabilitar el captcha

--- cypress/integration/RegistroBimbau.js
+++ cypress/integration/RegistroBimbau.js
@@ -2,27 +2,24 @@
    /// <reference types="cypress" /> 
    describe('Registro', function(){
    
-    it('Registro exitoso en Bimbau', function(){cy.wait(2000);
+    it('Registro exitoso en Bimbau', function(){
     cy.visit('https://app.bimbau.co/');
     cy.wait(2000);
-    cy.get(':nth-child(4) > .header-container > .login-container > .btn-register').click();cy.wait(2000);
+    cy.get(':nth-child(4) > .header-container > .login-container > .btn-register').click();
     cy.get('.toast__close-button > svg').click();//cerrar pop up de cookies
-    cy.wait(2000);
-    cy.get(':nth-child(1) > .field > .ui > input').type('angeldavid55@gmail.com');cy.wait(2000);
+    cy.get(':nth-child(1) > .field > .ui > input').type('angeldavid55@gmail.com');
     cy.get(':nth-child(2) > .field > .ui').click({force: true});cy.wait(2000);
     cy.get('.visible > :nth-child(2) > .text').click({force: true});cy.wait(2000);
-    cy.get(':nth-child(3) > .field > .ui > input').type('1063283967',{force: true});cy.wait(2000);
-    cy.get(':nth-child(4) > .field > .ui > input').type('Angel',{force: true});cy.wait(2000);
-    cy.get(':nth-child(5) > .field > .ui > input').type('Marzola',{force: true});cy.wait(2000);
-    cy.get('.equal > :nth-child(1) > input').type('3127016780',{force: true});cy.wait(2000);
-    cy.get('.equal > :nth-child(2) > input').type('7746670',{force: true});cy.wait(2000);
-    cy.get(':nth-child(3) > input').type('333',{force: true});cy.wait(2000);
-    cy.get('.mt-4 > .field > .ui > input').type('Vendedor',{force: true});cy.wait(2000);
+    cy.get(':nth-child(3) > .field > .ui > input').type('1063283967',{force: true});
+    cy.get(':nth-child(4) > .field > .ui > input').type('Angel',{force: true});
+    cy.get(':nth-child(5) > .field > .ui > input').type('Marzola',{force: true});
+    cy.get('.equal > :nth-child(1) > input').type('3127016780',{force: true});
+    cy.get('.equal > :nth-child(2) > input').type('7746670',{force: true});
+    cy.get(':nth-child(3) > input').type('333',{force: true});
+    cy.get('.mt-4 > .field > .ui > input').type('Vendedor',{force: true});
   
 
-    function cambiar(){
-      
-        };
+    
     cy.wait(2000);
   
    cy.get('iframe')
